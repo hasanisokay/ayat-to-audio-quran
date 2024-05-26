90382f1c34245f0d9e8c7c066bad0cdd3aaeb74f
hasanisokay <hasanisokay@gmail.com>
2024-05-26 16:20:31 +0600

    removed unnecessary packages

--- src/app/utils/generatePDF.js
+++ src/app/utils/generatePDF.js
@@ -3,23 +3,20 @@ import html2pdf from 'html2pdf.js';
 
 const generatePDF = () => {
   const input = document.getElementById('arabic-content');
-  const contentHeight = input.clientHeight; // Height of your content
+  const contentHeight = input.clientHeight; 
 
   const pageWidth = 210 * 72 / 25.4; // Convert mm to points
   const pageHeight = 297 * 72 / 25.4; // Convert mm to points
   const bottomMargin = 20; // Bottom margin in points
   const topMargin = 20; // Top margin in points
-
   // Calculate remaining space on the page after considering content height and margins
   const remainingSpace = pageHeight - contentHeight - bottomMargin - topMargin;
-
-  // Adjust content height to include space at the beginning and end
   input.style.marginTop = `${topMargin}px`;
   input.style.marginBottom = `${bottomMargin}px`;
 
   html2pdf(input, {
     margin: 10,
-    filename: 'download.pdf',
+    filename: 'ayats.pdf',
     image: { type: 'jpeg', quality: 0.98 },
     html2canvas: { dpi: 192, letterRendering: true },
     jsPDF: { unit: 'pt', format: 'a4', orientation: 'portrait' }
